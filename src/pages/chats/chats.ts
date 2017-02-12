29ad87e6ce6e60862c1e9900eb1524f4932b54ab
Corneezius <cfr2116@columbia.edu>
2017-02-12 02:51:41 -0500

    add models for chat and message, then import those models o chat typescript file

--- src/pages/chats/chats.ts
+++ src/pages/chats/chats.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
+// add observable to use stream of data for view
 import { Observable } from 'rxjs';
+// moment is an esssential package for data fabrication
 import * as moment from 'moment';
+import { Chat, MessageType } from '../../models';
 
 @Component({
   templateUrl: 'chats.html'
 })
 export class ChatsPage {
-  chats: Observable<any[]>;
+  chats: Observable<Chat[]>;
 
   constructor() {
     this.chats = this.findChats();
 
   }
 
-  private findChats(): Observable<any[]> {
+  private findChats(): Observable<Chat[]> {
     return Observable.of([
       {
         _id: '0',
@@ -21,7 +24,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
         lastMessage: {
           content: 'You on your way?',
-          createdAt: moment().subtract(1, 'hours').toDate()
+          createdAt: moment().subtract(1, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -30,7 +34,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
         lastMessage: {
           content: 'Hey, it\'s me',
-          createdAt: moment().subtract(2, 'hours').toDate()
+          createdAt: moment().subtract(2, 'hours').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -39,7 +44,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
         lastMessage: {
           content: 'I should buy a boat',
-          createdAt: moment().subtract(1, 'days').toDate()
+          createdAt: moment().subtract(1, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -48,7 +54,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
         lastMessage: {
           content: 'Look at my mukluks!',
-          createdAt: moment().subtract(4, 'days').toDate()
+          createdAt: moment().subtract(4, 'days').toDate(),
+          type: MessageType.TEXT
         }
       },
       {
@@ -57,7 +64,8 @@ export class ChatsPage {
         picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
         lastMessage: {
           content: 'This is wicked good ice cream.',
-          createdAt: moment().subtract(2, 'weeks').toDate()
+          createdAt: moment().subtract(2, 'weeks').toDate(),
+          type: MessageType.TEXT
         }
       }
     ]);
